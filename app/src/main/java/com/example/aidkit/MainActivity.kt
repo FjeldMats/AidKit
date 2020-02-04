cf04abbc9a03cccfd180b5bdcb8b4964d6274356
matsfj <fjeldmats@gmail.com>
2020-02-04 16:49:13 +0100

    linking activitys

--- app/src/main/java/com/example/aidkit/MainActivity.kt
+++ app/src/main/java/com/example/aidkit/MainActivity.kt
@@ -1,13 +1,36 @@
 package com.example.aidkit
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1 = findViewById<Button>(R.id.HelseBtn)
+        val btn2 = findViewById<Button>(R.id.SosialtBtn)
+        val btn3 = findViewById<Button>(R.id.OkonomiBtn)
+
+
+        val HelseActivity = Intent(this, HelseActivity::class.java)
+        val SosialtActivity = Intent(this, SosialtActivity::class.java)
+        val OkonomiActivity = Intent(this, OkonomiActivity::class.java)
+
+        btn1.setOnClickListener{
+            startActivity(HelseActivity)
+        }
+
+        btn2.setOnClickListener{
+            startActivity(SosialtActivity)
+        }
+
+        btn3.setOnClickListener{
+            startActivity(OkonomiActivity)
+        }
 
     }
 }
--- app/src/main/java/com/example/aidkit/OkonomiActivity.kt
+++ app/src/main/java/com/example/aidkit/OkonomiActivity.kt
@@ -1,12 +1,17 @@
 package com.example.aidkit
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class OkonomiActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_okonomi)
+
+
     }
 }
